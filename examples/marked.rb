2e7bbf8c699cab4e30cad1c76c16721e01352d12
Piotr Murach <pmurach@gmail.com>
2018-01-24 21:34:26 +0100

    Change example to use new api call

--- examples/marked.rb
+++ examples/marked.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tty-markdown'
 
 path = File.join(__dir__, 'example.md')
-out = TTY::Markdown.parse(File.read(path), colors: 256)
+out = TTY::Markdown.parse_file(path, colors: 256)
 
 puts out
