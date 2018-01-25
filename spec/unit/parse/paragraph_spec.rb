683f6a171bf396b1e30cd49034c446983c53b8b7
Piotr Murach <pmurach@gmail.com>
2018-01-25 23:58:38 +0100

    Add specs for paragraphs conversion

---
+++ spec/unit/parse/paragraph_spec.rb
@@ -0,0 +1,38 @@
+# fronze_string_literal: true
+
+RSpec.describe TTY::Markdown do
+  it "converts multiline paragraphs" do
+    markdown =<<-TEXT
+This is a first paragraph
+that spans two lines.
+
+And this is a next one.
+    TEXT
+    parsed = TTY::Markdown.parse(markdown)
+    expect(parsed).to eq([
+      "This is a first paragraph",
+      "that spans two lines.",
+      "",
+      "And this is a next one.\n"
+    ].join("\n"))
+  end
+
+  xit "converts multiline pragraphs within header section" do
+
+    markdown =<<-TEXT
+### header
+This is a first paragraph
+that spans two lines.
+
+And this is a next one.
+    TEXT
+    parsed = TTY::Markdown.parse(markdown)
+    expect(parsed).to eq([
+      "    \e[36;1mheader\e[0m",
+      "    This is a first paragraph",
+      "    that spans two lines.",
+      "",
+      "    And this is a next one.\n"
+    ].join("\n"))
+  end
+end
