c068ff1cb04a37903c0ea548da4e4617343b3c6a
Piotr Murach <pmurach@gmail.com>
2018-01-25 23:56:51 +0100

    Add specs to cover edge cases

--- spec/unit/parse/list_spec.rb
+++ spec/unit/parse/list_spec.rb
@@ -24,6 +24,50 @@ RSpec.describe TTY::Markdown do
     ].join("\n"))
   end
 
+  it "indents unordered list" do
+    markdown =<<-TEXT
+### header
+- Item 1
+  - Item 2
+  - Item 3
+    - Item 4
+    - Item 5
+- Item 6
+    TEXT
+    parsed = TTY::Markdown.parse(markdown)
+    expect(parsed).to eq([
+      "    \e[36;1mheader\e[0m",
+      "    #{pastel.yellow(symbols[:bullet])} Item 1",
+      "      #{pastel.yellow(symbols[:bullet])} Item 2",
+      "      #{pastel.yellow(symbols[:bullet])} Item 3",
+      "        #{pastel.yellow(symbols[:bullet])} Item 4",
+      "        #{pastel.yellow(symbols[:bullet])} Item 5",
+      "    #{pastel.yellow(symbols[:bullet])} Item 6\n"
+    ].join("\n"))
+  end
+
+  xit "indents unordered list with multiline content" do
+    markdown =<<-TEXT
+### header
+- First multiline
+  Item 1
+  - Second multiline
+    Item 2
+  - Item 3
+- Item 4
+    TEXT
+    parsed = TTY::Markdown.parse(markdown)
+    expect(parsed).to eq([
+      "    \e[36;1mheader\e[0m",
+      "    #{pastel.yellow(symbols[:bullet])} First multiline",
+      "    Item 1",
+      "      #{pastel.yellow(symbols[:bullet])} Second multiline",
+      "    Item 2",
+      "      #{pastel.yellow(symbols[:bullet])} Item 3",
+      "    #{pastel.yellow(symbols[:bullet])} Item 4\n",
+    ].join("\n"))
+  end
+
   it "convert ordered numbered list of nested items" do
     markdown =<<-TEXT
 1. Item 1
