aa46f65c6216d0c4707972bf6eecccee1dbb8b12
Piotr Murach <pmurach@gmail.com>
2018-03-17 21:07:02 +0000

    Change to restrict output width

--- examples/marked.rb
+++ examples/marked.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/tty-markdown'
 
 path = File.join(__dir__, 'example.md')
-out = TTY::Markdown.parse_file(path, colors: 256)
+out = TTY::Markdown.parse_file(path, colors: 256, width: 80)
 
 puts out
