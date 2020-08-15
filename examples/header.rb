669439416e93d95af33a2eba1f06ede6628ee031
Piotr Murach <pmurach@gmail.com>
2020-08-15 17:50:14 +0100

    Add example of different header levels

---
+++ examples/header.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+# Header 1
+
+I try all things, I achieve what I can.
+
+## Header 2
+
+I try all things, I achieve what I can.
+
+### Header 3
+
+I try all things, I achieve what I can.
+
+#### Header 4
+
+I try all things, I achieve what I can.
+
+##### Header 5
+
+I try all things, I achieve what I can.
+
+###### Header 6
+
+I try all things, I achieve what I can.
+TEXT
+
+print TTY::Markdown.parse(markdown)
