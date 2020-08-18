138d7255b1316bb2a7e46062a60965c2e9c413d2
Piotr Murach <pmurach@gmail.com>
2020-08-18 23:21:29 +0100

    Add emphasis example

---
+++ examples/emphasis.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Emphasis
+
+This is **bold**
+
+This is *italic*
+
+TEXT
+
+print TTY::Markdown.parse(markdown)
