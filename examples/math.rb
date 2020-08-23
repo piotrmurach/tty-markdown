ec898e908d77935e66f5218b93a82411c7ef104d
Piotr Murach <pmurach@gmail.com>
2020-08-23 15:08:34 +0100

    Add math element example

---
+++ examples/math.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Math formula inline
+
+Wave form: $$ nλ = dsinθ $$
+
+## Math formula block
+
+Wave form:
+
+$$ nλ = dsinθ $$
+
+TEXT
+
+print TTY::Markdown.parse(markdown)
