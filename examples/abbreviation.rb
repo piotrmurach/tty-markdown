3795a15f5def22780777d0fb0b953f3acfe9093a
Piotr Murach <pmurach@gmail.com>
2020-08-23 16:07:00 +0100

    Add abbreviation example

---
+++ examples/abbreviation.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Abbreviation
+
+write HTML page
+
+*[HTML]: Hyper Text Markup Language
+TEXT
+
+print TTY::Markdown.parse(markdown)
