4ce86e1e4293e6d0e7abc6b42a8458c3f08b6c0a
Piotr Murach <pmurach@gmail.com>
2020-08-23 14:15:33 +0100

    Add various link examples

---
+++ examples/link.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Inline link
+
+[TTY Toolkit](https://ttytoolkit.org)
+
+## Inline link with title
+
+[TTY site](https://ttytoolkit.org "TTY Toolkit")
+
+## Inline text matching link
+
+[https://ttytoolkit.org](https://ttytoolkit.org)
+
+## Inline link reference
+
+[TTY Toolkit][1]
+
+## Email link
+
+[Email me](mailto:test@example.com)
+
+[1]: https://ttytoolkit.org
+
+TEXT
+
+print TTY::Markdown.parse(markdown)
