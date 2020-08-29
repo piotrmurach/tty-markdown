db5897604c8fb64da4645cc924c5915908d22578
Piotr Murach <pmurach@gmail.com>
2020-08-29 15:12:38 +0100

    Add footnotes example

---
+++ examples/footnote.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Footnotes
+
+It is not down on any map[^foo]; true places[^bar] never are.
+
+[^foo]: A diagrammatic representation of an area of land or sea.
+[^bar]: A particular position, point, or area in space; a location.
+
+## Footnote With Multiline Content
+
+A single line footnote,[^1] and a longer one.[^multiline]
+
+[^1]: A single line footnote.
+
+[^multiline]: A footnote with multiple paragraphs and code.
+
+    Second paragraph of this footnote.
+
+    `{ foo: :bar }`
+
+    Paragraph to finish long description.
+TEXT
+
+print TTY::Markdown.parse(markdown)
