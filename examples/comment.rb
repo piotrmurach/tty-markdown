d128a5006d6c3e7194d71b3a754c7d0c2faace96
Piotr Murach <pmurach@gmail.com>
2020-08-30 19:49:38 +0100

    Add XML comment formatting example

---
+++ examples/comment.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Comment
+
+<!-- Single line comment -->
+
+## Comment Block
+
+<!--
+A comment that spans more than one line
+and explains important information
+-->
+
+TEXT
+
+print TTY::Markdown.parse(markdown)
