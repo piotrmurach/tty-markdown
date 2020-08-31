8b12bdb4816beb56aaf2ee05d7c06237a7ed5437
Piotr Murach <pmurach@gmail.com>
2020-08-31 14:38:53 +0100

    Add supported HTML elements example

---
+++ examples/html.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "../lib/tty-markdown"
+
+markdown =<<-TEXT
+
+## Deleted
+
+<del>done</del> made a mistake
+
+## Newline
+
+content<br>
+
+## Image
+
+<img width="130" src="https://github.com/piotrmurach/tty/raw/master/images/tty.png" alt="tty logo" />
+
+TEXT
+
+print TTY::Markdown.parse(markdown)
