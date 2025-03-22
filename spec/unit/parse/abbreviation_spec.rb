c29ceba8c8b0ceea13f0c4d21670103d3ae43d99
Piotr Murach <pmurach@gmail.com>
2025-03-22 20:28:24 +0100

    Add a test for converting an abbreviation without a description

--- spec/unit/parse/abbreviation_spec.rb
+++ spec/unit/parse/abbreviation_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Markdown, ".parse" do
-  it "abbreviates markdown" do
+  it "converts an abbreviation with a description" do
     markdown = <<-TEXT
 write HTML page
 
@@ -12,9 +12,20 @@ write HTML page
     expect(parsed).to eq("write HTML(Hyper Text Markup Language) page\n\n")
   end
 
-  it "indents abbreviations correctly" do
+  it "converts an abbreviation without a description" do
     markdown = <<-TEXT
-### header
+write HTML page
+
+*[HTML]:
+    TEXT
+    parsed = described_class.parse(markdown)
+
+    expect(parsed).to eq("write HTML page\n\n")
+  end
+
+  it "indents an abbreviation after the heading" do
+    markdown = <<-TEXT
+### Heading
 
 write HTML page
 
@@ -23,7 +34,7 @@ write HTML page
     parsed = described_class.parse(markdown, color: :always)
 
     expect(parsed).to eq([
-      "    \e[36;1mheader\e[0m\n",
+      "    \e[36;1mHeading\e[0m\n",
       "    write HTML(Hyper Text Markup Language) page\n\n"
     ].join("\n"))
   end
