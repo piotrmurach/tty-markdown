74092c1796ca5ac81beaf1c85c1d9459b861e0f0
Piotr Murach <pmurach@gmail.com>
2025-03-22 21:55:24 +0100

    Add tests for converting any HTML element with text or child elements

--- spec/unit/parse/html_spec.rb
+++ spec/unit/parse/html_spec.rb
@@ -14,6 +14,19 @@ RSpec.describe TTY::Markdown, ".parse" do
     expect(parsed).to eq("\e[33mSome\e[0m text \e[33;1mcontent\e[0m")
   end
 
+  it "converts the <span> element with text content" do
+    parsed = described_class.parse("<span>Some text content</span>")
+
+    expect(parsed).to eq("Some text content\n")
+  end
+
+  it "converts the <span> element with child elements" do
+    markdown = "<span><em>Some</em> text <strong>content</strong></span>"
+    parsed = described_class.parse(markdown, color: :always)
+
+    expect(parsed).to eq("\e[33mSome\e[0m text \e[33;1mcontent\e[0m\n")
+  end
+
   it "supports del html element" do
     markdown = <<-TEXT
 <del>done</del> made a mistake
