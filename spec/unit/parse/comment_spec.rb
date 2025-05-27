0f2ba479c5c69d0bd0054e1935b43ace5ea80729
Piotr Murach <pmurach@gmail.com>
2025-05-27 21:36:33 +0200

    Change the comment conversion tests to improve formatting

--- spec/unit/parse/comment_spec.rb
+++ spec/unit/parse/comment_spec.rb
@@ -1,113 +1,127 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Markdown, ".parse" do
-  it "converts xml comment within paragraph" do
-    markdown = <<-TEXT
-text before
-<!-- TODO: this is a comment -->
-text after
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+  context "when HTML" do
+    it "converts a single-line comment within a paragraph" do
+      markdown = <<-TEXT
+Text before.
+<!-- A single-line comment. -->
+Text after.
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "text before",
-      "\e[90m# TODO: this is a comment \e[0m",
-      "text after\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "Text before.",
+        "\e[90m# A single-line comment. \e[0m",
+        "Text after.\n"
+      ].join("\n"))
+    end
 
-  it "converts multiline xml comment within paragraph" do
-    markdown = <<-TEXT
-text before
+    it "converts a multiline comment within a paragraph" do
+      markdown = <<-TEXT
+Text before.
 <!--
-TODO: this is a comment
-that spans two lines
+A multiline comment
+that spans two lines.
 -->
-text after
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+Text after.
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "text before",
-      "\e[90m# TODO: this is a comment\e[0m",
-      "\e[90m# that spans two lines\e[0m",
-      "text after\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "Text before.",
+        "\e[90m# A multiline comment\e[0m",
+        "\e[90m# that spans two lines.\e[0m",
+        "Text after.\n"
+      ].join("\n"))
+    end
 
-  it "converts inline xml comment with indent inside paragraph" do
-    markdown = <<-TEXT
-### Header
+    it "converts a single-line comment within an indented paragraph" do
+      markdown = <<-TEXT
+### Heading
 
-text before
-<!-- TODO: this is a comment -->
-text after
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+Text before.
+<!-- A single-line comment. -->
+Text after.
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "    \e[36;1mHeader\e[0m",
-      "",
-      "    text before",
-      "    \e[90m# TODO: this is a comment \e[0m",
-      "    text after\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "    \e[36;1mHeading\e[0m",
+        "",
+        "    Text before.",
+        "    \e[90m# A single-line comment. \e[0m",
+        "    Text after.\n"
+      ].join("\n"))
+    end
 
-  it "converts multiline xml comment with indent inside paragraph" do
-    markdown = <<-TEXT
-### Header
+    it "converts a multiline comment within an indented paragraph" do
+      markdown = <<-TEXT
+### Heading
 
-text before
+Text before.
 <!--
-TODO: this is a comment
-that spans two lines
+A multiline comment
+that spans two lines.
 -->
-text after
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+Text after.
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "    \e[36;1mHeader\e[0m",
-      "",
-      "    text before",
-      "    \e[90m# TODO: this is a comment\e[0m",
-      "    \e[90m# that spans two lines\e[0m",
-      "    text after\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "    \e[36;1mHeading\e[0m",
+        "",
+        "    Text before.",
+        "    \e[90m# A multiline comment\e[0m",
+        "    \e[90m# that spans two lines.\e[0m",
+        "    Text after.\n"
+      ].join("\n"))
+    end
 
-  it "converts xml comment with indent without any context" do
-    markdown = <<-TEXT
-### Header
+    it "converts a single-line comment after the heading" do
+      markdown = <<-TEXT
+### Heading
 
-<!-- TODO: this is a comment -->
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+<!-- A single-line comment. -->
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "    \e[36;1mHeader\e[0m",
-      "",
-      "    \e[90m# TODO: this is a comment \e[0m\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "    \e[36;1mHeading\e[0m",
+        "",
+        "    \e[90m# A single-line comment. \e[0m\n"
+      ].join("\n"))
+    end
 
-  it "converts multiline xml comment with indent without any context" do
-    markdown = <<-TEXT
-### Header
+    it "converts a multiline comment after the heading" do
+      markdown = <<-TEXT
+### Heading
 
 <!--
-TODO: this is a comment
-that spans two lines
+A multiline comment
+that spans two lines.
 -->
-    TEXT
-    parsed = described_class.parse(markdown, color: :always, symbols: :unicode)
+      TEXT
+      parsed = described_class.parse(
+        markdown, color: :always, symbols: :unicode
+      )
 
-    expect(parsed).to eq([
-      "    \e[36;1mHeader\e[0m",
-      "",
-      "    \e[90m# TODO: this is a comment\e[0m",
-      "    \e[90m# that spans two lines\e[0m\n"
-    ].join("\n"))
+      expect(parsed).to eq([
+        "    \e[36;1mHeading\e[0m",
+        "",
+        "    \e[90m# A multiline comment\e[0m",
+        "    \e[90m# that spans two lines.\e[0m\n"
+      ].join("\n"))
+    end
   end
 end
