642ed0fb77cf43a8e64107f02e9cc1ca34fcacb2
Piotr Murach <pmurach@gmail.com>
2025-06-02 23:00:01 +0200

    Change the TTY::Markdown::Parser to improve formatting

--- lib/tty/markdown/parser.rb
+++ lib/tty/markdown/parser.rb
@@ -5,19 +5,30 @@ require "kramdown/parser/kramdown"
 module TTY
   module Markdown
     class Parser < Kramdown::Parser::Kramdown
-      def initialize(source, options)
-        super
-
-        { codeblock_fenced: :codeblock_fenced_ext }.each do |current, replacement|
-          @block_parsers[@block_parsers.index(current)] = replacement
-        end
-      end
+      FENCED_CODEBLOCK_MATCH = /
+        ^[ ]{0,3}(([~`]){3,})\s*?((\S+?)(?:\?\S*)?)?\s*?\n
+        (.*?)
+        ^[ ]{0,3}\1\2*\s*?\n
+      /mx.freeze
 
       FENCED_CODEBLOCK_START = /^[ ]{0,3}[~`]{3,}/.freeze
-      FENCED_CODEBLOCK_MATCH = /^[ ]{0,3}(([~`]){3,})\s*?((\S+?)(?:\?\S*)?)?\s*?\n(.*?)^[ ]{0,3}\1\2*\s*?\n/m.freeze
+      private_constant :FENCED_CODEBLOCK_START
 
-      define_parser(:codeblock_fenced_ext, FENCED_CODEBLOCK_START, nil,
-                    "parse_codeblock_fenced")
+      define_parser(:codeblock_fenced_extension, FENCED_CODEBLOCK_START, nil,
+                    :parse_codeblock_fenced)
+
+      def initialize(source, options)
+        super
+        replace_fenced_codeblock_parser
+      end
+
+      private
+
+      def replace_fenced_codeblock_parser
+        @block_parsers[
+          @block_parsers.index(:codeblock_fenced)
+        ] = :codeblock_fenced_extension
+      end
     end # Parser
   end # Markdown
 end # TTY
