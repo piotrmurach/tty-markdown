81ab9e4570c9bf8944ac1c219e3cc3478b13aeb5
Piotr Murach <pmurach@gmail.com>
2025-06-03 22:20:46 +0200

    Change the TTY::Markdown::Parser to improve YARD documentation

--- lib/tty/markdown/parser.rb
+++ lib/tty/markdown/parser.rb
@@ -4,19 +4,43 @@ require "kramdown/parser/kramdown"
 
 module TTY
   module Markdown
+    # Responsible for parsing standard and extended Markdown syntax
+    #
+    # @api private
     class Parser < Kramdown::Parser::Kramdown
+      # The fenced code block pattern
+      #
+      # @return [Regexp]
+      #
+      # @api private
       FENCED_CODEBLOCK_MATCH = /
         ^[ ]{0,3}(([~`]){3,})\s*?((\S+?)(?:\?\S*)?)?\s*?\n
         (.*?)
         ^[ ]{0,3}\1\2*\s*?\n
       /mx.freeze
 
+      # The fenced code block start pattern
+      #
+      # @return [Regexp]
+      #
+      # @api private
       FENCED_CODEBLOCK_START = /^[ ]{0,3}[~`]{3,}/.freeze
       private_constant :FENCED_CODEBLOCK_START
 
       define_parser(:codeblock_fenced_extension, FENCED_CODEBLOCK_START, nil,
                     :parse_codeblock_fenced)
 
+      # Create a {TTY::Markdown::Parser} instance
+      #
+      # @example
+      #   parser = TTY::Markdown::Parser.new("# TTY Toolkit", {})
+      #
+      # @param [String] source
+      #   the Markdown source
+      # @param [Hash] options
+      #   the parsing options
+      #
+      # @api private
       def initialize(source, options)
         super
         replace_fenced_codeblock_parser
@@ -24,6 +48,11 @@ module TTY
 
       private
 
+      # Replace the fenced code block parser
+      #
+      # @return [void]
+      #
+      # @api private
       def replace_fenced_codeblock_parser
         @block_parsers[
           @block_parsers.index(:codeblock_fenced)
