2c3b9aefc7ae6921bec99a76dad4b351e01d2ec1
Piotr Murach <pmurach@gmail.com>
2025-06-06 22:23:36 +0200

    Change the TTY::Markdown::Highlighter to remove custom language guessing

--- lib/tty/markdown/highlighter.rb
+++ lib/tty/markdown/highlighter.rb
@@ -8,35 +8,6 @@ module TTY
     #
     # @api private
     module Highlighter
-      # Return all available language lexers
-      #
-      # @return [Array[String]]
-      #
-      # @api private
-      def available_lexers
-        Rouge::Lexer.all.sort_by(&:tag).inject([]) do |names, lexer|
-          names << lexer.tag
-          if lexer.aliases.any?
-            lexer.aliases.each { |a| names << a }
-          end
-          names
-        end
-      end
-      module_function :available_lexers
-
-      # Guess langauge from code snippet
-      #
-      # @return [String, nil]
-      #
-      # @api private
-      def guess_lang(code)
-        start_line = code.lines[0]
-        if available_lexers.include?(start_line.strip.downcase)
-          start_line.strip.downcase
-        end
-      end
-      module_function :guess_lang
-
       # Highlight code snippet
       #
       # @param [String] code
@@ -52,7 +23,6 @@ module TTY
       # @api public
       def highlight(code, mode: 256, lang: nil, enabled: nil,
                     color: ->(line) { line })
-        lang = guess_lang(code) if lang.nil?
         lexer = Rouge::Lexer.find_fancy(lang, code) || Rouge::Lexers::PlainText
 
         if enabled == false
