be24e9104e3b5d9ecd461f39a31726724df4e071
Piotr Murach <pmurach@gmail.com>
2025-07-27 18:10:02 +0200

    Change the header conversion tests to improve formatting

--- spec/unit/parse/heading_spec.rb
+++ spec/unit/parse/heading_spec.rb
@@ -1,64 +1,65 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Markdown, ".parse" do
-  it "converts top level header" do
-    parsed = described_class.parse("Header1\n======", color: :always)
+  context "when Markdown" do
+    it "converts the level 1 heading when color is disabled" do
+      parsed = described_class.parse("Heading1\n========", color: :never)
 
-    expect(parsed).to eq("\e[36;1;4mHeader1\e[0m\n")
-  end
+      expect(parsed).to eq("Heading1\n")
+    end
 
-  it "disables top level header coloring" do
-    parsed = described_class.parse("Header1\n======", color: :never)
+    it "converts the level 1 heading" do
+      parsed = described_class.parse("Heading1\n========", color: :always)
 
-    expect(parsed).to eq("Header1\n")
-  end
+      expect(parsed).to eq("\e[36;1;4mHeading1\e[0m\n")
+    end
 
-  it "converts headers" do
-    headers = <<-TEXT
-# Header1
-header1 content
+    it "converts level 1, 2 and 3 headings" do
+      markdown = <<-TEXT
+# Heading1
+Content after the level 1 heading.
 
-## Header2
-header2 content
+## Heading2
+Content after the level 2 heading.
 
-### Header3
-header3 content
-    TEXT
-    parsed = described_class.parse(headers, color: :always)
+### Heading3
+Content after the level 3 heading.
+      TEXT
+      parsed = described_class.parse(markdown, color: :always)
 
-    expect(parsed).to eq([
-      "\e[36;1;4mHeader1\e[0m",
-      "header1 content",
-      "",
-      "  \e[36;1mHeader2\e[0m",
-      "  header2 content",
-      "",
-      "    \e[36;1mHeader3\e[0m",
-      "    header3 content\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "\e[36;1;4mHeading1\e[0m",
+        "Content after the level 1 heading.",
+        "",
+        "  \e[36;1mHeading2\e[0m",
+        "  Content after the level 2 heading.",
+        "",
+        "    \e[36;1mHeading3\e[0m",
+        "    Content after the level 3 heading.\n"
+      ].join("\n"))
+    end
 
-  it "indents within the specified width" do
-    parsed = described_class.parse(
-      "### Header3\n#{"x" * 21}", color: :always, width: 20
-    )
+    it "converts the heading followed by content within the allowed width" do
+      markdown = "### Heading3\n#{"x" * 21}"
+      parsed = described_class.parse(markdown, color: :always, width: 20)
 
-    expect(parsed).to eq([
-      "    \e[36;1mHeader3\e[0m",
-      "    xxxxxxxxxxxxxxxx",
-      "    xxxxx\n"
-    ].join("\n"))
-  end
+      expect(parsed).to eq([
+        "    \e[36;1mHeading3\e[0m",
+        "    xxxxxxxxxxxxxxxx",
+        "    xxxxx\n"
+      ].join("\n"))
+    end
 
-  it "indents long header within the specified width" do
-    header = "### It is not down on any map; true places never are."
-    parsed = described_class.parse(header, color: :always, width: 20)
+    it "converts the long heading within the allowed width" do
+      markdown = "### It is not down on any map; true places never are."
+      parsed = described_class.parse(markdown, color: :always, width: 20)
 
-    expect(parsed).to eq([
-      "    \e[36;1mIt is not down \e[0m",
-      "    \e[36;1mon any map; \e[0m",
-      "    \e[36;1mtrue places \e[0m",
-      "    \e[36;1mnever are.\e[0m\n"
-    ].join("\n"))
+      expect(parsed).to eq([
+        "    \e[36;1mIt is not down \e[0m",
+        "    \e[36;1mon any map; \e[0m",
+        "    \e[36;1mtrue places \e[0m",
+        "    \e[36;1mnever are.\e[0m\n"
+      ].join("\n"))
+    end
   end
 end
