502a537e323d7fc28e72d888574dbb000e35ab91
Piotr Murach <pmurach@gmail.com>
2026-02-05 19:23:24 +0100

    Change the heading conversion tests to improve formatting

--- spec/unit/parse/heading_spec.rb
+++ spec/unit/parse/heading_spec.rb
@@ -2,118 +2,122 @@
 
 RSpec.describe TTY::Markdown, ".parse" do
   context "when Markdown" do
-    it "converts the level 1 heading when color is disabled" do
-      parsed = described_class.parse("Heading1\n========", color: :never)
+    context "when ATX" do
+      it "converts level 1 to 3 headings" do
+        markdown = <<-TEXT
+# Heading 1
+Text after the level 1 heading.
 
-      expect(parsed).to eq("Heading1\n")
+## Heading 2
+Text after the level 2 heading.
+
+### Heading 3
+Text after the level 3 heading.
+        TEXT
+        parsed = described_class.parse(markdown, color: :always)
+
+        expect(parsed).to eq([
+          "\e[36;1;4mHeading 1\e[0m",
+          "Text after the level 1 heading.",
+          "",
+          "  \e[36;1mHeading 2\e[0m",
+          "  Text after the level 2 heading.",
+          "",
+          "    \e[36;1mHeading 3\e[0m",
+          "    Text after the level 3 heading.\n"
+        ].join("\n"))
+      end
+
+      it "converts level 1 to 3 headings with the custom heading1 style" do
+        markdown = <<-TEXT
+# Heading 1
+Text after the level 1 heading.
+
+## Heading 2
+Text after the level 2 heading.
+
+### Heading 3
+Text after the level 3 heading.
+        TEXT
+        parsed = described_class.parse(
+          markdown, color: :always, theme: {heading1: %i[blue]}
+        )
+
+        expect(parsed).to eq([
+          "\e[34mHeading 1\e[0m",
+          "Text after the level 1 heading.",
+          "",
+          "  \e[36;1mHeading 2\e[0m",
+          "  Text after the level 2 heading.",
+          "",
+          "    \e[36;1mHeading 3\e[0m",
+          "    Text after the level 3 heading.\n"
+        ].join("\n"))
+      end
+
+      it "converts level 1 to 3 headings with the custom header style" do
+        markdown = <<-TEXT
+# Heading 1
+Text after the level 1 heading.
+
+## Heading 2
+Text after the level 2 heading.
+
+### Heading 3
+Text after the level 3 heading.
+        TEXT
+        parsed = described_class.parse(
+          markdown, color: :always, theme: {header: %i[blue]}
+        )
+
+        expect(parsed).to eq([
+          "\e[36;1;4mHeading 1\e[0m",
+          "Text after the level 1 heading.",
+          "",
+          "  \e[34mHeading 2\e[0m",
+          "  Text after the level 2 heading.",
+          "",
+          "    \e[34mHeading 3\e[0m",
+          "    Text after the level 3 heading.\n"
+        ].join("\n"))
+      end
+
+      it "converts a heading followed by text within the allowed width" do
+        markdown = "### Heading\n#{"x" * 21}"
+        parsed = described_class.parse(markdown, color: :always, width: 20)
+
+        expect(parsed).to eq([
+          "    \e[36;1mHeading\e[0m",
+          "    xxxxxxxxxxxxxxxx",
+          "    xxxxx\n"
+        ].join("\n"))
+      end
+
+      it "converts a heading within the allowed width" do
+        markdown = "### It is not down on any map; true places never are."
+        parsed = described_class.parse(markdown, color: :always, width: 20)
+
+        expect(parsed).to eq([
+          "    \e[36;1mIt is not down \e[0m",
+          "    \e[36;1mon any map; \e[0m",
+          "    \e[36;1mtrue places \e[0m",
+          "    \e[36;1mnever are.\e[0m\n"
+        ].join("\n"))
+      end
     end
 
-    it "converts the level 1 heading" do
-      parsed = described_class.parse("Heading1\n========", color: :always)
+    context "when Setext" do
+      it "converts a level 1 heading when color is disabled" do
+        parsed = described_class.parse("Heading\n=======", color: :never)
 
-      expect(parsed).to eq("\e[36;1;4mHeading1\e[0m\n")
-    end
+        expect(parsed).to eq("Heading\n")
+      end
 
-    it "converts level 1, 2 and 3 headings" do
-      markdown = <<-TEXT
-# Heading1
-Content after the level 1 heading.
+      it "converts a level 1 heading" do
+        parsed = described_class.parse("Heading\n=======", color: :always)
 
-## Heading2
-Content after the level 2 heading.
-
-### Heading3
-Content after the level 3 heading.
-      TEXT
-      parsed = described_class.parse(markdown, color: :always)
-
-      expect(parsed).to eq([
-        "\e[36;1;4mHeading1\e[0m",
-        "Content after the level 1 heading.",
-        "",
-        "  \e[36;1mHeading2\e[0m",
-        "  Content after the level 2 heading.",
-        "",
-        "    \e[36;1mHeading3\e[0m",
-        "    Content after the level 3 heading.\n"
-      ].join("\n"))
-    end
-
-    it "converts level 1, 2 and 3 headings with the custom heading1 style" do
-      markdown = <<-TEXT
-# Heading1
-Content after the level 1 heading.
-
-## Heading2
-Content after the level 2 heading.
-
-### Heading3
-Content after the level 3 heading.
-      TEXT
-      parsed = described_class.parse(
-        markdown, color: :always, theme: {heading1: :blue}
-      )
-
-      expect(parsed).to eq([
-        "\e[34mHeading1\e[0m",
-        "Content after the level 1 heading.",
-        "",
-        "  \e[36;1mHeading2\e[0m",
-        "  Content after the level 2 heading.",
-        "",
-        "    \e[36;1mHeading3\e[0m",
-        "    Content after the level 3 heading.\n"
-      ].join("\n"))
-    end
-
-    it "converts level 1, 2 and 3 headings with the custom header style" do
-      markdown = <<-TEXT
-# Heading1
-Content after the level 1 heading.
-
-## Heading2
-Content after the level 2 heading.
-
-### Heading3
-Content after the level 3 heading.
-      TEXT
-      parsed = described_class.parse(
-        markdown, color: :always, theme: {header: :blue}
-      )
-
-      expect(parsed).to eq([
-        "\e[36;1;4mHeading1\e[0m",
-        "Content after the level 1 heading.",
-        "",
-        "  \e[34mHeading2\e[0m",
-        "  Content after the level 2 heading.",
-        "",
-        "    \e[34mHeading3\e[0m",
-        "    Content after the level 3 heading.\n"
-      ].join("\n"))
-    end
-
-    it "converts the heading followed by content within the allowed width" do
-      markdown = "### Heading3\n#{"x" * 21}"
-      parsed = described_class.parse(markdown, color: :always, width: 20)
-
-      expect(parsed).to eq([
-        "    \e[36;1mHeading3\e[0m",
-        "    xxxxxxxxxxxxxxxx",
-        "    xxxxx\n"
-      ].join("\n"))
-    end
-
-    it "converts the long heading within the allowed width" do
-      markdown = "### It is not down on any map; true places never are."
-      parsed = described_class.parse(markdown, color: :always, width: 20)
-
-      expect(parsed).to eq([
-        "    \e[36;1mIt is not down \e[0m",
-        "    \e[36;1mon any map; \e[0m",
-        "    \e[36;1mtrue places \e[0m",
-        "    \e[36;1mnever are.\e[0m\n"
-      ].join("\n"))
+        expect(parsed).to eq("\e[36;1;4mHeading\e[0m\n")
+      end
     end
   end
 end
