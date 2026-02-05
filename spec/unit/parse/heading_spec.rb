a22cb48e39b5206908fa7e9c1298af1128d2e23f
Piotr Murach <pmurach@gmail.com>
2026-02-05 20:41:17 +0100

    Add the level 1 to 6 ATX heading conversion tests

--- spec/unit/parse/heading_spec.rb
+++ spec/unit/parse/heading_spec.rb
@@ -3,7 +3,55 @@
 RSpec.describe TTY::Markdown, ".parse" do
   context "when Markdown" do
     context "when ATX" do
-      it "converts level 1 to 3 headings" do
+      it "converts a level 1 heading when color is disabled" do
+        parsed = described_class.parse("# Heading", color: :never)
+
+        expect(parsed).to eq("Heading\n")
+      end
+
+      it "converts a level 1 heading" do
+        parsed = described_class.parse("# Heading", color: :always)
+
+        expect(parsed).to eq("\e[36;1;4mHeading\e[0m\n")
+      end
+
+      it "converts a level 2 heading" do
+        parsed = described_class.parse("## Heading", color: :always)
+
+        expect(parsed).to eq("  \e[36;1mHeading\e[0m\n")
+      end
+
+      it "converts a level 3 heading" do
+        parsed = described_class.parse("### Heading", color: :always)
+
+        expect(parsed).to eq("    \e[36;1mHeading\e[0m\n")
+      end
+
+      it "converts a level 4 heading" do
+        parsed = described_class.parse("#### Heading", color: :always)
+
+        expect(parsed).to eq("      \e[36;1mHeading\e[0m\n")
+      end
+
+      it "converts a level 5 heading" do
+        parsed = described_class.parse("##### Heading", color: :always)
+
+        expect(parsed).to eq("        \e[36;1mHeading\e[0m\n")
+      end
+
+      it "converts a level 6 heading" do
+        parsed = described_class.parse("###### Heading", color: :always)
+
+        expect(parsed).to eq("          \e[36;1mHeading\e[0m\n")
+      end
+
+      it "converts an invalid level 7 heading" do
+        parsed = described_class.parse("####### Heading", color: :always)
+
+        expect(parsed).to eq("          \e[36;1m# Heading\e[0m\n")
+      end
+
+      it "converts level 1 to 6 headings" do
         markdown = <<-TEXT
 # Heading 1
 Text after the level 1 heading.
@@ -13,6 +61,15 @@ Text after the level 2 heading.
 
 ### Heading 3
 Text after the level 3 heading.
+
+#### Heading 4
+Text after the level 4 heading.
+
+##### Heading 5
+Text after the level 5 heading.
+
+###### Heading 6
+Text after the level 6 heading.
         TEXT
         parsed = described_class.parse(markdown, color: :always)
 
@@ -24,11 +81,20 @@ Text after the level 3 heading.
           "  Text after the level 2 heading.",
           "",
           "    \e[36;1mHeading 3\e[0m",
-          "    Text after the level 3 heading.\n"
+          "    Text after the level 3 heading.",
+          "",
+          "      \e[36;1mHeading 4\e[0m",
+          "      Text after the level 4 heading.",
+          "",
+          "        \e[36;1mHeading 5\e[0m",
+          "        Text after the level 5 heading.",
+          "",
+          "          \e[36;1mHeading 6\e[0m",
+          "          Text after the level 6 heading.\n"
         ].join("\n"))
       end
 
-      it "converts level 1 to 3 headings with the custom heading1 style" do
+      it "converts level 1 to 6 headings with the custom heading1 style" do
         markdown = <<-TEXT
 # Heading 1
 Text after the level 1 heading.
@@ -38,6 +104,15 @@ Text after the level 2 heading.
 
 ### Heading 3
 Text after the level 3 heading.
+
+#### Heading 4
+Text after the level 4 heading.
+
+##### Heading 5
+Text after the level 5 heading.
+
+###### Heading 6
+Text after the level 6 heading.
         TEXT
         parsed = described_class.parse(
           markdown, color: :always, theme: {heading1: %i[blue]}
@@ -51,11 +126,20 @@ Text after the level 3 heading.
           "  Text after the level 2 heading.",
           "",
           "    \e[36;1mHeading 3\e[0m",
-          "    Text after the level 3 heading.\n"
+          "    Text after the level 3 heading.",
+          "",
+          "      \e[36;1mHeading 4\e[0m",
+          "      Text after the level 4 heading.",
+          "",
+          "        \e[36;1mHeading 5\e[0m",
+          "        Text after the level 5 heading.",
+          "",
+          "          \e[36;1mHeading 6\e[0m",
+          "          Text after the level 6 heading.\n"
         ].join("\n"))
       end
 
-      it "converts level 1 to 3 headings with the custom header style" do
+      it "converts level 1 to 6 headings with the custom header style" do
         markdown = <<-TEXT
 # Heading 1
 Text after the level 1 heading.
@@ -65,6 +149,15 @@ Text after the level 2 heading.
 
 ### Heading 3
 Text after the level 3 heading.
+
+#### Heading 4
+Text after the level 4 heading.
+
+##### Heading 5
+Text after the level 5 heading.
+
+###### Heading 6
+Text after the level 6 heading.
         TEXT
         parsed = described_class.parse(
           markdown, color: :always, theme: {header: %i[blue]}
@@ -78,7 +171,16 @@ Text after the level 3 heading.
           "  Text after the level 2 heading.",
           "",
           "    \e[34mHeading 3\e[0m",
-          "    Text after the level 3 heading.\n"
+          "    Text after the level 3 heading.",
+          "",
+          "      \e[34mHeading 4\e[0m",
+          "      Text after the level 4 heading.",
+          "",
+          "        \e[34mHeading 5\e[0m",
+          "        Text after the level 5 heading.",
+          "",
+          "          \e[34mHeading 6\e[0m",
+          "          Text after the level 6 heading.\n"
         ].join("\n"))
       end
 
